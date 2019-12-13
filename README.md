062bf1a4ce6e7d29da91eb96795f889d1e15e79b
jbettenh <21023218+jbettenh@users.noreply.github.com>
2019-12-13 22:34:58 +0100

    added reference

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 December 13, 2019 - Initial commit
 
 ## References
+<https://levelup.gitconnected.com/the-easy-guide-to-python-command-line-arguments-96b4607baea1>
 
 ## License
 MIT License
--- py_trim.py
+++ py_trim.py
@@ -4,6 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser(description='A tutorial of argparse!')
 parser.add_argument("--a", default=1, help="This is the 'a' variable")
+group = parser.add_mutually_exclusive_group(required=True)
+
+group.add_argument('--c', action='store_true', help="This is the 'c' variable")
+group.add_argument('--d', action='store_true', help="This is the 'd' variable")
 
 args = parser.parse_args()
 a = args.a

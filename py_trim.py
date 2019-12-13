fe4bec915cd0967d046884b76ab5a5f859fbb753
jbettenh <21023218+jbettenh@users.noreply.github.com>
2019-12-13 21:49:34 +0100

    argparse tutorial

--- py_trim.py
+++ py_trim.py
@@ -3,5 +3,8 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+parser.add_argument("--a", default=1, help="This is the 'a' variable")
 
 args = parser.parse_args()
+a = args.a
+print(a)

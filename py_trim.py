5372ae3fbb4b39dcae69da3120e25bbceaedb510
jbettenh <21023218+jbettenh@users.noreply.github.com>
2019-12-13 21:25:01 +0100

    import argparse

---
+++ py_trim.py
@@ -0,0 +1,7 @@
+#! python 3
+# This program will rename folders and files based on given standard.
+import argparse
+
+parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+
+args = parser.parse_args()

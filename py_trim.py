2d0852e25f8583295be38d0169c6625c56837087
jbettenh <21023218+jbettenh@users.noreply.github.com>
2019-12-13 23:37:52 +0100

    two groups

--- py_trim.py
+++ py_trim.py
@@ -3,11 +3,16 @@
 import argparse
 
 parser = argparse.ArgumentParser(description='A tutorial of argparse!')
-parser.add_argument("--a", default=1, help="This is the 'a' variable")
-group = parser.add_mutually_exclusive_group(required=True)
 
-group.add_argument('--c', action='store_true', help="This is the 'c' variable")
-group.add_argument('--d', action='store_true', help="This is the 'd' variable")
+group = parser.add_mutually_exclusive_group()
+group.add_argument('--rs',  help="This removes spaces")
+group.add_argument('--csu', help="This changes spaces to underscores")
+group.add_argument('--csm', help="This changes spaces to minuses")
+
+group_case = parser.add_mutually_exclusive_group()
+group_case.add_argument('-l', action='store_true', help="This makes all letters lower case")
+group_case.add_argument('-u', action='store_true', help="This makes all letters upper case")
+group_case.add_argument('-c', action='store_true', help="This makes the words camel case")
 
 args = parser.parse_args()
 a = args.a

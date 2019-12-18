f656b1face548351a9e52ea48e3c25d853c7219f
jbettenh <21023218+jbettenh@users.noreply.github.com>
2019-12-18 22:41:17 +0100

    argument testing

--- py_trim.py
+++ py_trim.py
@@ -1,7 +1,33 @@
 #! python 3
-# This program will rename folders and files based on given standard.
+# This script will rename files into a defined format.
 import os
+import sys
 import argparse
+
+
+
+
+def arguments():
+    parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-rs', action='store_true', help="This removes spaces")
+    group.add_argument('-su', action='store_true', help="This changes spaces to underscores")
+    group.add_argument('-sm', action='store_true', help="This changes spaces to minuses")
+
+    group_case = parser.add_mutually_exclusive_group()
+    group_case.add_argument('-l', action='store_true', help="This makes all letters lower case")
+    group_case.add_argument('-u', action='store_true', help="This makes all letters upper case")
+    group_case.add_argument('-c', action='store_true', help="This makes the words camel case")
+
+    args = parser.parse_args()
+    print(args)
+    print(args.rs)
+    print(args.su)
+    print(args.sm)
+    print(args.l)
+    print(args.u)
+    print(args.c)
 
 
 def main():
@@ -11,23 +37,9 @@ def main():
 
         os.rename('test.txt','test01.txt')
 
-def arguments():
-    parser = argparse.ArgumentParser(description='A tutorial of argparse!')
-
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--rs', help="This removes spaces")
-    group.add_argument('--csu', help="This changes spaces to underscores")
-    group.add_argument('--csm', help="This changes spaces to minuses")
-
-    group_case = parser.add_mutually_exclusive_group()
-    group_case.add_argument('-l', action='store_true', help="This makes all letters lower case")
-    group_case.add_argument('-u', action='store_true', help="This makes all letters upper case")
-    group_case.add_argument('-c', action='store_true', help="This makes the words camel case")
-
-    args = parser.parse_args()
-    a = args.a
-    print(a)
 
 if __name__ == "__main__":
-    # is this needed?
-    main()
+    #input command line parameters
+    #get path
+    #get format
+    arguments()
